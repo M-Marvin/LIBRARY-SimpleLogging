5385cd7dbce9b93676841e83896128b2f3d57170
M-Marvin <69386758+M-Marvin@users.noreply.github.com>
2025-01-13 19:06:17 +0100

    Implemented colored option for StreamLogger

--- SimpleLogging/src/main/java/de/m_marvin/simplelogging/impl/StreamLogger.java
+++ SimpleLogging/src/main/java/de/m_marvin/simplelogging/impl/StreamLogger.java
@@ -8,17 +8,23 @@ import java.nio.charset.StandardCharsets;
 import de.m_marvin.simplelogging.LogLevel;
 
 public class StreamLogger extends SimpleLogger {
-	
-	public final OutputStream stream;
-	public final Charset charset;
 
+	protected final boolean colored;
+	protected final OutputStream stream;
+	protected final Charset charset;
+	
 	public StreamLogger(OutputStream stream) {
 		this(stream, StandardCharsets.UTF_8);
 	}
-	
+
 	public StreamLogger(OutputStream stream, Charset charset) {
+		this(stream, charset, false);
+	}
+	
+	public StreamLogger(OutputStream stream, Charset charset, boolean colored) {
 		this.stream = stream;
 		this.charset = charset;
+		this.colored = colored;
 	}
 	
 	public OutputStream getStream() {
@@ -32,7 +38,9 @@ public class StreamLogger extends SimpleLogger {
 	@Override
 	public void print(LogLevel level, String msg) {
 		try {
+			if (this.colored) this.stream.write(level.getFormat().getBytes(this.charset));
 			this.stream.write(msg.getBytes(this.charset));
+			if (this.colored) this.stream.write(level.getFormatReset().getBytes(this.charset));
 			this.stream.write('\n');
 		} catch (IOException e) {
 			e.printStackTrace();
